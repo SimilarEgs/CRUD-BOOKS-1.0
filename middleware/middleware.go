55050d22fe8d00d0ffcf62451ca572ac9dde31a5
SimilarEgs <newfrontierorder@gmail.com>
2022-05-24 15:55:37 +0300

    working

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,5 +1,45 @@
 package middleware
 
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/SimilarEgs/CURD-BOOKS/db"
+	"github.com/SimilarEgs/CURD-BOOKS/models"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+//establishing a DB connection
+
+func createDBConnection() *sql.DB{
+	//loading environment file
+	err := godotenv.Load(".env")
+	if err != nil{
+		log.Fatalf("[Error] .env file didn't load")
+	}
+	//open DB connection
+	db, err := sql.Open("postgres", os.Getenv("POSTGRES_URL"))
+	if err != nil{
+		panic(err)
+	}
+	//check connection
+	err = db.Ping()
+	if err != nil{
+		panic(err)
+	}
+
+	fmt.Println("[Info] DB was successfully connected")
+	return db
+}
+
+
+
 func getBookById(){}
 
 func getAllBooks() {}
--- router/router.go
+++ router/router.go
@@ -8,11 +8,11 @@ import (
 func Router() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/book{id}", middleware.getBookById())
-	router.HandleFunc("/book", middleware.getAllBooks())
-	router.HandleFunc()
-	router.HandleFunc()
-	router.HandleFunc()
+	router.HandleFunc("/book/{id}", middleware.getBookById).Methods("GET")
+	router.HandleFunc("/book", middleware.getAllBooks).Methods("GET")
+	router.HandleFunc("/book/{id}", middleware.deleteBookById).Methods("DELETE")
+	router.HandleFunc("book/{id}", middleware.updateBookById).Methods("PUT")
+	router.HandleFunc("/book", middleware.updateBookById).Methods("POST")
 
 	return
 }
