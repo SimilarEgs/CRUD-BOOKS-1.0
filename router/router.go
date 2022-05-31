5a4421a07b82343ca3ace595ecdd99471e368239
Lex <newfrontierorder@gmail.com>
2022-05-31 17:04:04 +0300

    Update router.go

--- router/router.go
+++ router/router.go
@@ -9,11 +9,11 @@ import (
 func Router() *mux.Router {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/book/{id}", middleware.GetBookById).Methods("GET", "OPTIONS")
-	router.HandleFunc("/book", middleware.GetAllBooks).Methods("GET", "OPTIONS")
-	router.HandleFunc("/book/{id}", middleware.DeleteBookById).Methods("DELETE", "OPTIONS")
-	router.HandleFunc("/book/{id}", middleware.UpdateBookById).Methods("PUT", "OPTIONS")
-	router.HandleFunc("/book", middleware.CreateBook).Methods("POST", "OPTIONS")
+	router.HandleFunc("/book/{id}", middleware.GetBookById).Methods("GET"
+	router.HandleFunc("/book", middleware.GetAllBooks).Methods("GET")
+	router.HandleFunc("/book/{id}", middleware.DeleteBookById).Methods("DELETE")
+	router.HandleFunc("/book/{id}", middleware.UpdateBookById).Methods("PUT")
+	router.HandleFunc("/book", middleware.CreateBook).Methods("POST")
 
 	return router
 }
